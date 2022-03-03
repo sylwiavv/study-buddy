f0d788b6dfdd0bc6370801af3c3070a7235e0777
Sylwia Waligóra <sylwia.waligora@ageno.pl>
2022-03-03 18:41:25 +0100

    Resolve ERR_OSSL_EVP_UNSUPPORTED

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -6,7 +6,7 @@ export const theme = {
     lightGrey: '#F7F8FA',
     grey: '#C0C7D6',
     darkGrey: '#737C8E',
-    black: '#111111',
+    black: '#060d1c',
     success: '#8FCB81',
     error: '#FF8383',
     warning: '#E1D888',
